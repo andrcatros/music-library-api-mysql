b80d9848f6318a3a2c18f91ff4cdb489c8fb0191
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-07-14 10:23:51 +0100

    Resolved issue with bulkCreate in Album controller, can now create multiple entries accurate if req.body is an array of json objects.

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -3,15 +3,17 @@ const { Album } = require('../models');
 
 exports.createAlbum = (req, res) => {
     const { artistId } = req.params;
-    const body = req.body;
   
     Artist.findByPk(artistId).then(foundArtist => {
       if (!foundArtist) {
         res.status(404).json({ error: 'The artist could not be found.' });
-      } else if (req.body.length != 1) {         
-            Album.bulkCreate(req.body).then(albums => {
-                res.status(201).json({albums})
-            })
+      } else if (req.body[0]) {
+        const promiseArr = req.body.map(entry => {
+          Album.create(entry).then(album => album.setArtist(foundArtist))
+        });
+
+        Promise.all(promiseArr).catch(err => console.log('something failed to resolve', err))
+        .then(albums => res.status(201).json(albums))
         } else {
             Album.create({year:req.body.year, name:req.body.name}).then(album => {
                 album.setArtist(foundArtist).then(updatedAlbum => {

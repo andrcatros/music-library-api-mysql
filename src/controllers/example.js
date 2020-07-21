6cc9f659e0619da4baa01ef509384b26b153a63e
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-07-21 21:21:57 +0100

    Streamlined controllers.

---
+++ src/controllers/example.js
@@ -0,0 +1,14 @@
+// import model
+
+
+/* controllers should be formatted as exports.controllerName = (req, res) => {    
+
+}; */
+
+// controllers for POST endpoints
+
+// controllers for GET endpoints
+
+// controllers for PATCH endpoints
+
+// controllers for DELETE endpoints

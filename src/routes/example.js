03f6304d8a3ddecc15be6cb6b9d3ed8261e6c909
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-07-21 21:31:35 +0100

    Streamlined routes folder.

---
+++ src/routes/example.js
@@ -0,0 +1,14 @@
+// import controller 
+const exampleControllers = require('./controllers/example');
+
+//
+module.exports = (app) => {
+        // post routes
+    
+        // get routes
+    
+        // patch routes
+    
+        // delete routes
+
+}
